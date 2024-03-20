6b79e554e035efdd0b176a9e125e90ced4401738
marlin dalpozzo <marlin.dalpozzo@gmail.com>
2024-03-20 16:54:59 -0700

    initial.

---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,8 @@
+export default function AppHeader() {
+  return (
+    <header className="w-full h-20 sm:h-32 bg-green-200 border-b-2 border-b-gray-400 flex flex-row">
+      <div className="font-bold text-lg">branding here</div>
+      <div className="w-10 sm:w-14 h-10 sm:h-14 bg-purple-400 rounded-full" />
+    </header>
+  )
+}
