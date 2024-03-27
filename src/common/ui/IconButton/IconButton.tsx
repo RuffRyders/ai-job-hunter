cf3561f049d39be7b5502c83ef39358fe09473a8
Kyle Bedell <kgbedell@gmail.com>
2024-03-27 16:47:56 -0700

    style: switch to cn from interpolation

--- src/common/ui/IconButton/IconButton.tsx
+++ src/common/ui/IconButton/IconButton.tsx
@@ -1,11 +1,15 @@
 'use client'
 
+import { cn } from '@/common/utils/style/cn'
 import { Button, ButtonProps } from 'react-aria-components'
 
-export function IconButton({ children, className, ...rest }: ButtonProps) {
+export function IconButton({ children, className = '', ...rest }: ButtonProps) {
   return (
     <Button
-      className={`pointer-events-auto rounded-full hover:bg-gray-200 cursor-pointer p-2 text-sm font-semibold text-black disabled:bg-slate-100 disabled:text-slate-500 ${className}`}
+      className={cn(
+        'pointer-events-auto rounded-full hover:bg-gray-200 cursor-pointer p-2 text-sm font-semibold text-black disabled:bg-slate-100 disabled:text-slate-500',
+        className,
+      )}
       {...rest}
     >
       {children}
