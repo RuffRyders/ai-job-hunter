c171609b98841fedc5e7266f61ebdfc0e704c9b3
marlin dalpozzo <marlin.dalpozzo@gmail.com>
2024-04-03 14:17:34 -0700

    use react-aria-components.

--- src/features/header/ui/UserMenu.tsx
+++ src/features/header/ui/UserMenu.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef, useState } from 'react'
 import { signOut } from '@/features/auth/serverActions/signOut'
 import LoadingOverlay from '@/common/ui/LoadingOverlay'
 import { cn } from '@/common/utils/style/cn'
+import { Key, Menu, MenuItem } from 'react-aria-components'
 
 const PADDING_RIGHT = 'pr-6'
 const PADDING_LEFT = 'pl-6'
@@ -56,29 +57,19 @@ export const UserMenu = ({ isOpen, onClose, userData }: UserMenuProps) => {
     }
   }, [isOpen, onClose])
 
-  useEffect(() => {
-    if (isOpen) {
-      // Focus the first item when the menu opens
-      menuRef.current?.querySelector('li')?.focus()
-    }
-  }, [isOpen])
-
   const handleKeyDown = (event: React.KeyboardEvent) => {
-    const currentFocus = document.activeElement as HTMLElement
-
-    if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
-      event.preventDefault() // Prevent page scrolling
-      if (event.key === 'ArrowDown') {
-        ;(currentFocus.nextElementSibling as HTMLElement)?.focus()
-      } else {
-        ;(currentFocus.previousElementSibling as HTMLElement)?.focus()
-      }
-    } else if (event.key === 'Enter') {
-      // Simulate click on the currently focused element
-      currentFocus.click()
-    } else if (event.key === 'Escape') {
+    if (event.key === 'Escape' && isOpen) {
       onClose()
     }
+  }
+
+  const handleAction = async (action: Key) => {
+    if (action === 'settings') {
+      handleSettings()
+    } else if (action === 'signout') {
+      await handleSignOut()
+    }
+    onClose()
   }
 
   if (!isOpen) {
@@ -87,6 +78,7 @@ export const UserMenu = ({ isOpen, onClose, userData }: UserMenuProps) => {
 
   return (
     <div
+      onKeyDown={handleKeyDown}
       ref={menuRef}
       className="bg-gray-100 w-56 overflow-hidden shadow-lg rounded-md border border-gray-200 z-50"
     >
@@ -100,22 +92,14 @@ export const UserMenu = ({ isOpen, onClose, userData }: UserMenuProps) => {
         <div className="text-sm font-bold truncate">{userData.email}</div>
       </div>
 
-      <ul>
-        <UserMenuItem
-          className={cn(PADDING_LEFT, PADDING_RIGHT)}
-          onClick={handleSettings}
-          onKeyDown={handleKeyDown}
-        >
+      <Menu aria-label="User Menu" onAction={handleAction}>
+        <UserMenuItem id="settings" className={cn(PADDING_LEFT, PADDING_RIGHT)}>
           Settings
         </UserMenuItem>
-        <UserMenuItem
-          className={cn(PADDING_LEFT, PADDING_RIGHT)}
-          onClick={handleSignOut}
-          onKeyDown={handleKeyDown}
-        >
+        <UserMenuItem id="signout" className={cn(PADDING_LEFT, PADDING_RIGHT)}>
           Log out
         </UserMenuItem>
-      </ul>
+      </Menu>
 
       <LoadingOverlay loading={loading} displayText="Signing out..." />
     </div>
@@ -124,28 +108,21 @@ export const UserMenu = ({ isOpen, onClose, userData }: UserMenuProps) => {
 
 interface UserMenuItemProps {
   children: React.ReactNode
-  onClick: () => void
-  onKeyDown: (event: React.KeyboardEvent) => void
   className?: string
+  id: string
 }
 
-const UserMenuItem = ({
-  children,
-  onClick,
-  onKeyDown,
-  className,
-}: UserMenuItemProps) => {
+const UserMenuItem = ({ children, id, className }: UserMenuItemProps) => {
   return (
-    <li
+    <MenuItem
+      aria-label={`Menu Item - ${id}`}
+      id={id}
       className={cn(
         'hover:bg-gray-200 cursor-pointer p-2 focus:outline-none focus:bg-gray-200',
         className,
       )}
-      tabIndex={0}
-      onClick={onClick}
-      onKeyDown={onKeyDown}
     >
       {children}
-    </li>
+    </MenuItem>
   )
 }
