4b460a342a1cad2d76623580ab72bb00dfa78624
Kyle Bedell <kgbedell@gmail.com>
2024-04-03 21:41:42 -0700

    feat(job-tracker): copy kanban example from dnd-kit

---
+++ src/features/jobTracker/ui/JobsKanbanView/components/Container/Container.tsx
@@ -0,0 +1,81 @@
+import React, { forwardRef } from 'react'
+
+import { Handle, Remove } from '../Item'
+
+import styles from './Container.module.scss'
+import { cn } from '@/common/utils/style/cn'
+
+export interface ContainerProps {
+  children: React.ReactNode
+  columns?: number
+  label?: string
+  style?: React.CSSProperties
+  horizontal?: boolean
+  hover?: boolean
+  handleProps?: React.HTMLAttributes<any>
+  scrollable?: boolean
+  shadow?: boolean
+  placeholder?: boolean
+  unstyled?: boolean
+  onClick?(): void
+  onRemove?(): void
+}
+
+export const Container = forwardRef<HTMLDivElement, ContainerProps>(
+  function Container(
+    {
+      children,
+      columns = 1,
+      handleProps,
+      horizontal,
+      hover,
+      onClick,
+      onRemove,
+      label,
+      placeholder,
+      style,
+      scrollable,
+      shadow,
+      unstyled,
+      ...props
+    }: ContainerProps,
+    ref: React.ForwardedRef<any>,
+  ) {
+    const Component = onClick ? 'button' : 'div'
+
+    return (
+      <Component
+        {...props}
+        ref={ref}
+        style={
+          {
+            ...style,
+            '--columns': columns,
+          } as React.CSSProperties
+        }
+        className={cn(
+          styles.Container,
+          unstyled && styles.unstyled,
+          horizontal && styles.horizontal,
+          hover && styles.hover,
+          placeholder && styles.placeholder,
+          scrollable && styles.scrollable,
+          shadow && styles.shadow,
+        )}
+        onClick={onClick}
+        tabIndex={onClick ? 0 : undefined}
+      >
+        {label ? (
+          <div className={styles.Header}>
+            {label}
+            <div className={styles.Actions}>
+              {onRemove ? <Remove onClick={onRemove} /> : undefined}
+              <Handle {...handleProps} />
+            </div>
+          </div>
+        ) : null}
+        {placeholder ? children : <ul>{children}</ul>}
+      </Component>
+    )
+  },
+)
---
+++ src/features/jobTracker/ui/JobsKanbanView/index.ts
@@ -0,0 +1,1 @@
+export { JobsKanbanView } from './JobsKanbanView'
